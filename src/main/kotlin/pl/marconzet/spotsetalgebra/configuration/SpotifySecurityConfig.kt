9768dd650e6d6c8a8eaf2ce6baeac8c1a6c867fa
MarconZet <marcin.zlakowski@gmail.com>
2021-01-03 21:25:22 +0100

    Fixed bootstrap authentication

--- src/main/kotlin/pl/marconzet/spotsetalgebra/configuration/SpotifySecurityConfig.kt
+++ src/main/kotlin/pl/marconzet/spotsetalgebra/configuration/SpotifySecurityConfig.kt
@@ -5,20 +5,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.AnyRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
 
 
 @EnableWebSecurity
 class SpotifySecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity?) {
         http {
+            
             authorizeRequests {
-                authorize("", permitAll)
-                authorize("/", permitAll)
-                authorize("/callback/**", permitAll)
-                authorize("/oauth2/authorization/**", permitAll)
-                authorize(anyRequest, authenticated)
+                authorize("/workspace/**", authenticated)
+                authorize(anyRequest, permitAll)
             }
-            oauth2Login {  }
+            oauth2Login { }
         }
     }
 }
